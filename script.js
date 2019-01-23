4777380595da74385f11d166e6de9e4ddf5a2cdb
Alline Coelho <allinemarjorie@gmail.com>
2019-01-22 22:53:54 -0200

    Change vars to lets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function escondeDiv(elemento) {
-	var selecionado = elemento.options[elemento.selectedIndex].value;
+	let selecionado = elemento.options[elemento.selectedIndex].value;
 	document.getElementById('finalidade-proprio0').style.display = 'none';
 
 	if (selecionado === 'proprio') {
@@ -12,18 +12,16 @@ function escondeDiv(elemento) {
 }
 
 function adicionaCampo() {
-	var materiais = document.getElementById("materiais");
-  var indiceAtual = materiais.rows.length;
-  var linhaAtual = materiais.insertRow(0);
+	let materiais = document.getElementById("materiais");
+	let indiceAtual = materiais.rows.length;
+	let linhaAtual = materiais.insertRow(0);
 
-  var codProdProt = linhaAtual.insertCell(-1);
+	let codProdProt = linhaAtual.insertCell(-1);
+	let descrMaterial = linhaAtual.insertCell(-1);
+	let numSerie = linhaAtual.insertCell(-1);
+	let qtd = linhaAtual.insertCell(-1);
+	let valorUnit = linhaAtual.insertCell(-1);
+	let valorTotal = linhaAtual.insertCell(-1);
+	let notaFiscalOrigem = linhaAtual.insertCell(-1);
 
-  var descrMaterial = linhaAtual.insertCell(-1);
-  var numSerie = linhaAtual.insertCell(-1);
-  var qtd = linhaAtual.insertCell(-1);
-  var valorUnit = linhaAtual.insertCell(-1);
-  var valorTotal = linhaAtual.insertCell(-1);
-  var notaFiscalOrigem = linhaAtual.insertCell(-1);
-
-  console.log("aaa");
 }
